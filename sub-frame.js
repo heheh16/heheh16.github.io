597c9cf6ec720b609e24a510d8085b7ce62e772e
Nikita Guzeev <nguzeev@sinergo.ru>
2020-08-20 18:05:05 +0700

    fafawa

---
+++ sub-frame.js
@@ -0,0 +1,91 @@
+Cookies.defaults = {
+  expires: 365,
+  path: '',
+  domain: window.location.hostname.split('.').slice(-2).join('.'),
+};
+
+hashSetStorage = async() => {
+  const advanced_finger = await getFinger('localhost', true);
+  hashPlace.innerHTML = advanced_finger
+};
+hashSetStorageUnsupported = async() => {
+    const advanced_finger = await getFinger('localhost', false);
+    hashPlace.innerHTML = advanced_finger
+};
+
+var hashPlace = document.getElementById('a1');
+var btn = document.getElementById('test');
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function storageAccessAPISupported() {
+  return (
+    'hasStorageAccess' in document &&
+    'requestStorageAccess' in document
+  );
+}
+
+function accessStorage(fn) {
+  document.hasStorageAccess()
+    .then(function (hasAccess){
+      if (hasAccess) {
+        console.info('Storage API Access already granted');
+        fn();
+        return;
+      }
+
+      console.info('no existing Storage API Access ...');
+      console.info('requesting Storage API Access ...');
+
+      document.requestStorageAccess()
+        .then(function () {
+          console.info('Storage API Access granted.');
+          fn();
+          return;
+        }, function (){
+          console.warn('Storage API Access denied.');
+        });
+    }, function (reason) {
+      console.warn('something went wrong ...');
+      console.error(reason);
+    });
+}
+
+function init() {
+  var data = Cookies.get('finger_advanced') || '(none)';
+  var updateEvent = new CustomEvent('hashPlace:updated', {
+    bubbles: true,
+    cancelable: false,
+    detail: {
+      hashPlace: data,
+    },
+  });
+  hashPlace.dispatchEvent(updateEvent);
+  btn.click()
+}
+
+function attachEventHandlers() {
+  if (!storageAccessAPISupported()) {
+      console.log('BBBBBBBBBBBB');
+      btn.addEventListener('click',  hashSetStorageUnsupported);
+  }
+  else {
+      console.log('AAAAAAAAAAAAAAAA');
+      btn.addEventListener('click', accessStorage.bind(null, hashSetStorage));
+  }
+}
+
+function onReady() {
+    console.log('TTTT');
+    attachEventHandlers();
+    init();
+}
+
+ready(onReady);
+
---
+++ utils.js
@@ -0,0 +1,31 @@
+setFingerToStorage = (finger, script, storageAccessApi) => {
+    Cookies.set('finger_'+script, finger);
+    localStorage.setItem('finger_'+script, finger);
+    sessionStorage.setItem('finger_'+script, finger);
+    // if(storageAccessApi) {
+    //     var data = Cookies.get('finger_'+script) || '(none)';
+    //     var updateEvent = new CustomEvent('uid:updated', {
+    //         bubbles: true,
+    //         cancelable: false,
+    //         detail: {
+    //             finger_advanced: data,
+    //         },
+    //     });
+    //     hashPlace.dispatchEvent(updateEvent);
+    // }
+};
+
+
+sendDataToServ = (fingerprint, script, destination, storageAccessApi) => {
+    let old_finger;
+    if(storageAccessApi) {
+        old_finger = Cookies.get('finger_'+script) || null
+    }
+    const request_obj = {
+        "old_fingerprint": old_finger,
+        "current_fingerprint": fingerprint,
+    };
+    console.log('SEND DATA', request_obj)
+    // $.post(destination, request_obj);
+};
+
