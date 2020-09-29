fd052e1dec227c0b213303d3d21cdbab6a533700
heheh16 <nikitgzv@gmail.com>
2020-09-29 11:23:24 +0700

    fdawfaw

--- env_vars.js
+++ env_vars.js
@@ -1,4 +1,4 @@
 var SERVER_GEN = 'https://gitlab.tec47.com:4430/id/';
-var SERVER_DATA_TO = 'gitlab.tec47.com:4430/';
+var SERVER_DATA_TO = 'https://gitlab.tec47.com:4430/';
 var ELEMENT_FOR_HASH = 'a1';
 var VERSION = 214124141241241
