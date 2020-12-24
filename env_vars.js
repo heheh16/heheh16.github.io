a3198c29a07d1436f069dcff680bb6a1165d345c
heheh16 <nikitgzv@gmail.com>
2020-12-24 14:26:18 +0700

    fwafwa

--- env_vars.js
+++ env_vars.js
@@ -1,4 +1,4 @@
-var SERVER_GEN = "https://gitlab.tec47.com:4430/id/";
+var SERVER_GEN = "https://door.semantiqo.com/id/";
 var SERVER_DATA_TO = "https://gitlab.tec47.com:4430";
 var ELEMENT_FOR_HASH = "a1";
 var VERSION = 18;
