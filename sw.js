ef2cca8c975f9ccf3115dda99f280a9cf407a44e
heheh16 <nikitgzv@gmail.com>
2021-03-03 01:36:05 +0700

    fwafwafwa

--- sw.js
+++ sw.js
@@ -1,31 +1,67 @@
-self.addEventListener("install", function (event) {
-  console.log("AAAAAAAAAAAAAAAAAAAAAA");
-  event.waitUntil(
-    caches.open("v2").then(function (cache) {
-      return cache.addAll(["/"]);
-    })
-  );
+// let duplicateRequest = false;
+// self.addEventListener("install", function (event) {
+//   console.log("AAAAAAAAAAAAAAAAAAAAAA");
+//   event.waitUntil(
+//     caches.open("v2").then(function (cache) {
+//       return cache.addAll(["/"]);
+//     })
+//   );
+// });
+
+// addEventListener("fetch", (event) => {
+//   event.waitUntil(
+//     (async function () {
+//       if (
+//         event.request.url === "https://jsonplaceholder.typicode.com/todos/1" &&
+//         duplicateRequest === false
+//       ) {
+//         duplicateRequest = true;
+//         console.log("SEND REQUEST");
+//       } else {
+//         console.log("DUPLICATE");
+//       }
+// if (!event.clientId)
+//   return;
+
+// const client = await clients.get(event.clientId);
+
+// if (!client) return;
+
+// console.log("TTTTTTTTTTTTTTTT", event.request);
+// console.log("JJJJJJJJJJJJJJJJJ", a);
+// client.postMessage({
+//   msg: "Hey I just got a fetch from you!",
+//   url: event.request.url,
+// });
+//     })()
+//   );
+// });
+
+self.addEventListener("install", (event) => {
+  console.log("V1 installing…");
+
+  // cache a cat SVG
+  event.waitUntil(caches.open("static-v1").then((cache) => cache.add("/")));
 });
 
-addEventListener("fetch", (event) => {
-  event.waitUntil(
-    (async function () {
-      // Exit early if we don't have access to the client.
-      // Eg, if it's cross-origin.
-      if (!event.clientId) return;
+self.addEventListener("activate", (event) => {
+  console.log("V1 now ready to handle fetches!");
+});
 
-      // Get the client.
-      const client = await clients.get(event.clientId);
-      // Exit early if we don't get the client.
-      // Eg, if it closed.
-      if (!client) return;
+addEventListener("message", async (event) => {
+  // event is an ExtendableMessageEvent object
+  console.log(`The client sent me a message: ${event.data}`);
+  const resp = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const result = await resp.json();
+  event.source.postMessage(JSON.stringify(result));
+});
 
-      // Send a message to the client.
-      console.log("TTTTTTTTTTTTTTTT", event.request);
-      client.postMessage({
-        msg: "Hey I just got a fetch from you!",
-        url: event.request.url,
-      });
-    })()
-  );
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  // serve the cat SVG from the cache if the request is
+  // same-origin and the path is '/dog.svg'
+  if (url.origin == location.origin && url.pathname == "/dog.svg") {
+    event.respondWith(caches.match("/cat.svg"));
+  }
 });
