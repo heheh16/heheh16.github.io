647cb7ccdf01fb933202c6581ff6043a0205c0b5
heheh16 <nikitgzv@gmail.com>
2021-03-12 13:08:37 +0700

    fwafw

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-let installDate, id;
+let installDate;
 
 const setData = new Promise((resolve, reject) => {
-  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
-  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAA", installDate);
-  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAA", id);
-  const uuid = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(
-    /[018]/g,
-    function (c) {
-      return (
-        c ^
-        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-      ).toString(16);
-    }
-  );
-  id = uuid;
   installDate = new Date().toUTCString();
   resolve(true);
 });
@@ -25,5 +12,6 @@ self.addEventListener("install", function (event) {
 self.addEventListener("activate", function (event) {});
 
 addEventListener("message", (event) => {
-  event.source.postMessage(JSON.stringify({ SwDate: installDate, SwId: id }));
+  event.source.postMessage(JSON.stringify({ SwDate: installDate}));
 });
+
